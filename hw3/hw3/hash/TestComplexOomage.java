d201930d9b4dcf04f087c155530c8ba3e858fd15
liuyuchen <yuchen150211@gmail.com>
2019-10-24 15:46:55 +0800

    finish hw3

---
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -0,0 +1,62 @@
+package hw3.hash;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestComplexOomage {
+
+    @Test
+    public void testHashCodeDeterministic() {
+        ComplexOomage so = ComplexOomage.randomComplexOomage();
+        int hashCode = so.hashCode();
+        for (int i = 0; i < 100; i += 1) {
+            assertEquals(hashCode, so.hashCode());
+        }
+    }
+
+    /* This should pass if your OomageTestUtility.haveNiceHashCodeSpread
+       is correct. This is true even though our given ComplexOomage class
+       has a flawed hashCode. */
+    @Test
+    public void testRandomOomagesHashCodeSpread() {
+        List<Oomage> oomages = new ArrayList<Oomage>();
+        int N = 10000;
+
+        for (int i = 0; i < N; i += 1) {
+            oomages.add(ComplexOomage.randomComplexOomage());
+        }
+
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
+    }
+
+    /* TODO: Create a list of Complex Oomages called deadlyList
+     * that shows the flaw in the hashCode function.
+     */
+    @Test
+    public void testWithDeadlyParams() {
+        List<Oomage> deadlyList = new ArrayList<Oomage>();
+        /**
+         * we know the code will overflow if number exceeds 256e3
+         * consider the binary, 1111.hashCode() results in 256e4, thus, will be added to bucket 9 all the time
+         * */
+        for(int i = 0; i < 10; i++){
+            ArrayList<Integer> params = new ArrayList<Integer>();
+            for(int j = 0; j < 4; j++){
+                params.add(1);
+            }
+            ComplexOomage com = new ComplexOomage(params);
+            deadlyList.add(com);
+        }
+
+        assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
+    }
+
+    /** Calls tests for SimpleOomage. */
+    public static void main(String[] args) {
+        jh61b.junit.textui.runClasses(TestComplexOomage.class);
+    }
+}
